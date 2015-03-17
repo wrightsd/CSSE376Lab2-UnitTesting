05e6cb880ae668d63a60d24b6b155d98daf9cfd8
wrightsd <wrightsd@rose-hulman.edu>
2015-03-17 15:35:05 -0400

    Spencer Wright Lab 2 Submission

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,62 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+        private readonly DateTime StartDate = new DateTime(2015, 12, 2);
+        private readonly DateTime EndDate=new DateTime(2015,12,7);
+        private readonly int NumberOfMiles = 10;
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var newFlight = new Flight(StartDate, EndDate, NumberOfMiles);
+            Assert.IsNotNull(newFlight);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestExceptionForStartDateAfterEndDate()
+        {
+            new Flight(StartDate,new DateTime(2015,12,1),NumberOfMiles);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestExeptionForNegativeMiles()
+        {
+            new Flight(StartDate, EndDate, -4);
+        }
+
+        [Test()]
+        public void TestBasePrice()
+        {
+            var newFlight = new Flight(StartDate,EndDate,NumberOfMiles);
+            Assert.AreEqual(300,newFlight.getBasePrice());
+        }
+
+        [Test()]
+        public void TestEqualsIsSame()
+        {
+            var newFlight1 = new Flight(StartDate, EndDate, NumberOfMiles);
+            var newFlight2 = new Flight(StartDate,EndDate,NumberOfMiles);
+            Assert.True(newFlight1.Equals(newFlight2));
+
+        }
+
+        [Test()]
+        public void TestEqualsIsNotSameInDate()
+        {
+            var newFlight1 = new Flight(StartDate, EndDate, NumberOfMiles);
+            var newFlight2 = new Flight(new DateTime(2015,12,1), EndDate, NumberOfMiles);
+            Assert.False(newFlight1.Equals(newFlight2));
+        }
+
+        [Test()]
+        public void TestEqualsIsNotSameInMiles()
+        {
+            var newFlight1 = new Flight(StartDate, EndDate, NumberOfMiles);
+            var newFlight2 = new Flight(StartDate, EndDate, 12);
+            Assert.False(newFlight1.Equals(newFlight2));
+        }
+
 	}
 }
